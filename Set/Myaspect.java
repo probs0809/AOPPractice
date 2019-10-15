8f7908f8ec81875f1ffa5238db23adea9b9994c9
Prabodh Mayekar <prabodhmayekar@gmail.com>
2019-10-15 09:54:12 +0530

    set and get

--- Set/Myaspect.java
+++ Set/Myaspect.java
@@ -1,16 +1,28 @@
 aspect Myaspect{
-    pointcut p1():set(int Book.pages);
-    after() : p1(){
-        System.out.println("pages set");
+    // pointcut p1():set(int Book.pages);
+    // after() : p1(){
+    //     System.out.println("pages set");
+    // }
+
+    // pointcut p2():set(int Book.price);
+    // after() : p2(){
+    //     System.out.println("price set");
+    // }
+
+    // pointcut p3():set(String Book.author);
+    // after() : p3(){
+    //     System.out.println("author set");
+    // }
+
+    pointcut p4():get(* Book.*);
+    pointcut p5():set(* Book.*);
+    before() : p4(){
+        System.out.println("The value of");
+
     }
 
-    pointcut p2():set(int Book.price);
-    after() : p2(){
-        System.out.println("price set");
+    after() : p5(){
+        System.out.println("Value Initialised ");
     }
 
-    pointcut p3():set(String Book.author);
-    after() : p3(){
-        System.out.println("author set");
-    }
 }
